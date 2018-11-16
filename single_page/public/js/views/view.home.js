1e213af374c4d84bc41aad7ef9a9efc27ff1b2cf
tranChiVi0123 <idiots1230@gmail.com>
2018-11-16 07:36:26 +0700

    '1'

---
+++ single_page/public/js/views/view.home.js
@@ -0,0 +1,207 @@
+/*
+Name: 			View - Home
+Written by: 	Okler Themes - (http://www.okler.net)
+Theme Version:	5.7.2
+*/
+
+(function($) {
+
+	'use strict';
+
+	/*
+	Circle Slider
+	*/
+	if ($.isFunction($.fn.flipshow)) {
+		var circleContainer = $('#fcSlideshow');
+
+		if (circleContainer.get(0)) {
+			circleContainer.flipshow();
+
+			setTimeout(function circleFlip() {
+				circleContainer.data().flipshow._navigate(circleContainer.find('div.fc-right span:first'), 'right');
+				setTimeout(circleFlip, 3000);
+			}, 3000);
+		}
+	}
+
+	/*
+	Move Cloud
+	*/
+	if ($('.cloud').get(0)) {
+		var moveCloud = function() {
+			$('.cloud').animate({
+				'top': '+=20px'
+			}, 3000, 'linear', function() {
+				$('.cloud').animate({
+					'top': '-=20px'
+				}, 3000, 'linear', function() {
+					moveCloud();
+				});
+			});
+		};
+
+		moveCloud();
+	}
+
+	/*
+	Nivo Slider
+	*/
+	if ($.isFunction($.fn.nivoSlider)) {
+		$('#nivoSlider').nivoSlider({
+			effect: 'random',
+			slices: 15,
+			boxCols: 8,
+			boxRows: 4,
+			animSpeed: 500,
+			pauseTime: 3000,
+			startSlide: 0,
+			directionNav: true,
+			controlNav: true,
+			controlNavThumbs: false,
+			pauseOnHover: true,
+			manualAdvance: false,
+			prevText: 'Prev',
+			nextText: 'Next',
+			randomStart: false,
+			beforeChange: function(){},
+			afterChange: function(){},
+			slideshowEnd: function(){},
+			lastSlide: function(){},
+			afterLoad: function(){}
+		});
+	}
+
+}).apply(this, [jQuery]);
+/* ---- particles.js config ---- */
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+
+
+/* ---- stats.js config ---- */
+
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function() {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
